d548f2407605b31ce4cd828e2a79f3893cad2d56
Bernard Yohanna <benayoh@gmail.com>
2020-04-08 04:13:04 +0100

    initializing estimator

---
+++ src/estimator.js
@@ -0,0 +1,9 @@
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input,
+    impact: {},
+    severeImpact: {}
+  };
+
+export default covid19ImpactEstimator;
