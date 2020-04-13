dd9c1b46dd40ed459b8953b1c8ed2907e2565b41
Bernard Yohanna <benayoh@gmail.com>
2020-04-13 11:46:49 +0100

    start building

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,27 @@
-const covid19ImpactEstimator = (data) => {
-  const input = data;
-  return {
-    data: input,
-    impact: {},
-    severeImpact: {}
-  };
+let getFactor;
+const factor = (data) => {
+  if (data.periodType.trim().toLowerCase() === 'days') {
+    getFactor = Math.trunc((data.timeToElapse * 1) / 3);
+  } else if (data.periodType.trim().toLowerCase() === 'weeks') {
+    getFactor = Math.trunc((data.timeToElapse * 7) / 3);
+  } else if (data.periodType.trim().toLowerCase() === 'months') {
+    getFactor = Math.trunc((data.timeToElapse * 30) / 3);
+  } else {
+    getFactor = 1;
+  }
+  return getFactor;
+};
+
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** factor)
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** factor)
+  }
+});
 
 export default covid19ImpactEstimator;
